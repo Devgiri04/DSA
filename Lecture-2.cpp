b9d8589d0b285d270e109ce3f69f2990f3fa239e
Dev <devgiri96273@gmail.com>
2024-05-07 20:25:08 +0530

    Lecture

---
+++ Lecture-2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Namaste Duniya " << endl;
+
+    //^ DataTypes and variables
+
+    //^ Type Casting
+    char ch = 98;
+    cout << ch << endl;
+
+    int a = 'b';
+    cout << a << endl;
+    
+    //^ Unsigned Integer
+     
+     unsigned int c = 122;
+     cout << c << endl;
+}
---
+++ Lecture-3.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    /*
+    !Question 1: Number is Positive, Negative or Zero
+    int num;
+    cout << "Enter the Number- ";
+    cin >> num;
+
+    if (num == 0)
+    {
+        cout << "Number is zero" << endl;
+    }
+    else if (num < 0)
+    {
+        cout << "Number is negative" << endl;
+    }
+    else
+    {
+        cout << "Number is positive" << endl;
+    }
+    */
+
+
+    /*
+    !Question 2: Print from 1 to 10 using while loop
+
+    int i = 1;
+    while( i <= 10){
+        cout << i << " ";
+        i++;
+    }
+    */
+
+
+
+
+
+}
